f52d7cfc73ddd38da8272ce2749a11bd44a4bf68
Aviad Cohen <aviadc@liveperson.com>
2023-02-07 21:45:21 +0200

    list example works, from some reason...

--- src/main/java/puk/team/example/response/Main.java
+++ src/main/java/puk/team/example/response/Main.java
@@ -1,5 +1,8 @@
 package puk.team.example.response;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static <D, E> Result<D, E> createResult(D data, Class<E> type) {
@@ -8,8 +11,14 @@ public class Main {
         return res;
     }
 
+    public static <T> List<T> createListWithItem(T item) {
+        List<T> res = new ArrayList<>();
+        res.add(item);
+        return res;
+    }
+
     public static void main(String[] args) {
         Result<String, String> test = createResult("Test", String.class);
-
+        List<String> list = createListWithItem("BANANA");
     }
 }
